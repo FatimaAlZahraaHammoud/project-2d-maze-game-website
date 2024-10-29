e9811a49c272b470d02454baa884cddc2b1be7a0
unknown <fatimaalzahraahammoud@gmail.com>
2024-10-30 01:55:55 +0200

    Animations fixed and Collisions added

--- scripts/game.js
+++ scripts/game.js
@@ -5,30 +5,43 @@ class baseline extends Phaser.Scene{
   }
 
   create() {
-    this.initBackground()
-    this.initPlayer()
-    this.initAnimation()
-    this.initKeys()
+    this.initBackground();
+    this.initPlayer();
+    this.initAnimation();
+    this.initKeys();
+    this.initObjects();
   }
 
+  // initialize Backgrounds
   initBackground(){
     this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
     this.background.setOrigin(0, 0);
 
   }
 
+  // initialize Player
   initPlayer(){
-
     this.player1 = this.physics.add.sprite(config.width / 2 - 50, config.height / 2, "player");
     // sets the canvas to be a boundary
-    this.player1.setCollideWorldBou
+    this.player1.setCollideWorldBounds(true);
     this.player1.play("player_anim");
     this.player1.setInteractive();
+  }
 
+  // initialize objects
+  initObjects(){
+    this.obstaclesGroup = this.add.group();
+    this.tree = this.physics.add.sprite(200, 150, "tree");
+    this.tree.setImmovable(true);
+    this.tree.setSize(20, 20);
+    this.tree.setOffset(3, 3);
+    //this.rocks = this.physics.add.sprite(100, 150, "rocks");
+
+    this.obstaclesGroup.add(this.tree);
   }
   
+  // initialize Keys
   initKeys(){
-    
     // init keys
     this.cursors = this.input.keyboard.createCursorKeys();
     // W A S D
@@ -36,7 +49,7 @@ class baseline extends Phaser.Scene{
     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
-    // spacebar
+    // space bar
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
 
   }
@@ -44,17 +57,23 @@ class baseline extends Phaser.Scene{
   update() {
     this.movePlayer(this.player1);
     this.handleAttack(this.player1);
+    this.collidePlayerObjects();
   }
 
   // animation function
   initAnimation(){
-      // UP DOWN LEFT RIGHT
+
+    // UP DOWN LEFT RIGHT
+
+    // left
     this.anims.create({
       key: 'left',
       frames: this.anims.generateFrameNumbers('player', { start: 24, end: 29 }), 
       frameRate: 10,
       repeat: -1
     });
+
+    //right
     this.anims.create({
       key: 'right',
       frames: this.anims.generateFrameNumbers('player', { start: 24, end: 29 }), 
@@ -62,6 +81,7 @@ class baseline extends Phaser.Scene{
       repeat: -1
     });
 
+    //up
     this.anims.create({
       key: 'up',
       frames: this.anims.generateFrameNumbers('player', { start: 31, end: 35 }),
@@ -69,14 +89,15 @@ class baseline extends Phaser.Scene{
       repeat: -1
     });
 
+    //down
     this.anims.create({
       key: 'down',
       frames: this.anims.generateFrameNumbers('player', { start: 19, end: 23 }), 
       frameRate: 5,
       repeat: -1
     });
-    // Ending directions
 
+    // Ending directions
     this.anims.create({
       key: 'stop',
       frames: this.anims.generateFrameNumbers('player', { start: 0, end: 5 }), 
@@ -97,29 +118,34 @@ class baseline extends Phaser.Scene{
 
     player.setVelocity(0);
 
+    // moving player left
     if (this.cursors.left.isDown||this.keyA.isDown) {
       player.setVelocityX(-130);
-      if (player.anims.currentAnim?.key !== 'down') {
+      player.flipX = true;
+      if (player.anims.currentAnim?.key !== 'left') {
         player.anims.play('left', true);
       }
      
     }
     
+    // moving player right
     else if (this.cursors.right.isDown||this.keyD.isDown) {
       player.setVelocityX(130);
+      player.flipX = false;
       if (player.anims.currentAnim?.key !== 'right') {
         player.anims.play('right', true);
-    } 
+      } 
     }
 
+    // moving player up
     if (this.cursors.up.isDown||this.keyW.isDown) {
       player.setVelocityY(-130);
       if (player.anims.currentAnim?.key !== 'up') {
         player.anims.play('up', true);
+      }
     }
-
-    }
-  
+    
+    // moving player down
     else if (this.cursors.down.isDown||this.keyS.isDown) {
       player.setVelocityY(130);
       if (player.anims.currentAnim?.key !== 'down') {
@@ -128,15 +154,15 @@ class baseline extends Phaser.Scene{
 
     }
 
-    else{
-      player.setVelocityY(0);
-
+    // stopping player
+    if (player.body.velocity.x === 0 && player.body.velocity.y === 0) {
       if (player.anims.currentAnim?.key !== 'stop') {
-        player.anims.play('stop', true);
+          player.anims.play('stop', true);
       }
     }
   }
   
+  // attacking
   handleAttack(player){
     if(this.keySpace.isDown && !this.isAttacking){
       this.isAttacking = true
@@ -148,6 +174,12 @@ class baseline extends Phaser.Scene{
       })
     }
   }
+
+  // Collision between Player and Object
+  collidePlayerObjects(){
+    this.physics.collide(this.player1, this.obstaclesGroup);
+  }
+  
 }
 
 // menu
@@ -167,34 +199,37 @@ class MenuScene extends Phaser.Scene {
 
 
 
-    // SCENE1
+// SCENE1
 
-    class Scene1 extends Phaser.Scene {
-      constructor() {
-        super("bootGame");
-      }
+class Scene1 extends Phaser.Scene {
+  constructor() {
+    super("bootGame");
+  }
+
+  preload(){
+    this.load.image("background", "assets/tilesets/Grass_Middle.png");
     
-      preload(){
-        this.load.image("background", "assets/tilesets/Grass_Middle.png");
-        
-        this.load.spritesheet("player", "assets/characters/player.png",{
-          frameWidth: 48,
-          frameHeight: 48
-        });  
-      }
-     
-      create() {
-        this.add.text(20, 20, "Loading game...");
-        this.scene.start("playGame");
-      }
-    }
+    this.load.spritesheet("player", "assets/characters/player.png",{
+      frameWidth: 48,
+      frameHeight: 48
+    });  
+
+    this.load.image("tree", "assets/objects/Oak_Tree.png",{
+      frameWidth: 48,
+      frameHeight: 48
+    });  
+  }
+  
+  create() {
+    this.add.text(20, 20, "Loading game...");
+    this.scene.start("playGame");
+  }
+}
 
 class Scene2 extends baseline {
   constructor() {
     super("playGame");
   }
-
-
 }
 
 var config = {
@@ -209,6 +244,6 @@ var config = {
           debug: false
       }
     }
-  }
+}
   
-  var game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
